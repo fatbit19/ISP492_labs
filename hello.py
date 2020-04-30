78a12314692f24268cbbff20e9d75b7706f3d4e0
fatbit19 <64597035+fatbit19@users.noreply.github.com>
2020-04-30 15:35:40 +0100

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,10 @@
+# file: helloworld test program 
+
+# this clode bloacks prints out helloworld
+
+import datetime as dt 
+
+print ("\n Helloworld, my IDE is wokring just fine! \n ")
+dt.timedelta()
+
+print ('time')
